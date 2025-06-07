302c1f24049be643543b1836ebf7f4a23452c4f8
yohannabarbara <yohannabarbara666@gmail.com>
2025-06-07 20:26:22 -0300

    feat:tela seja um colaborador

--- frontend/src/pages/BeCollaborator/BeCollaborator.jsx
+++ frontend/src/pages/BeCollaborator/BeCollaborator.jsx
@@ -2,10 +2,42 @@ import './beCollaborator.css'
 
 function BeCollaborator() {
 
-    return (
+     return (
+        <div className='container'>
+            <form className='container-form collaborator'>
+                <h1 className='container-title'>Seja um Colaborador</h1>
+                  <h2 className='instructions'> Ajude-nos com a publicação de conteúdos!</h2>
+                <input
+                    className='container-input'
+                    placeholder='CPF'
+                    name='cpf'
+                    type='cpf'
+                />
 
-        <h1>Seja um colaborador</h1>
+                <input
+                    className='container-input'
+                    placeholder='Ocupação/Atuação'
+                    name='ocupação'
+                    type='text'
+                  
+                />
+                <input
+                    className='container-input'
+                    placeholder='Instituiçao'
+                    name='instituição'
+                    type='text'
+                  
+                />
 
+                <button
+                    className='container-button'
+                    type='button'
+                >
+                    Enviar
+                </button>
+                 <h2 className='instructions'> Vamos analisar os seus dados e enviaremos o resultado via e-mail. </h2>
+            </form>
+        </div>
     )
 
 }
